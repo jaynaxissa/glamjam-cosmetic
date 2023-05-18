b9b4dccc031659de9d8d15e9c1d6f025e24ca90b
jaynaxissa <jaynaxissa@gmail.com>
2023-05-18 13:30:41 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="home-container">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home;
